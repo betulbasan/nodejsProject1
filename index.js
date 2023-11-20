20ff013b4201ff6448c3e3ddf8909c90d36fc8bd
betulbasan <63635709+betulbasan@users.noreply.github.com>
2023-11-20 10:58:19 +0300

    little mistake

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express"); // import express module
 const app = express(); 
 
-const listener = server.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
     console.log("listening on port " + listener.address().port)
 })
